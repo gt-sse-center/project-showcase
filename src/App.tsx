2bb9d9d15b85d92b2493cc339690a1d8effdc3b9
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-03-13 12:28:17 -0400

    cleaning up unused bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import './App.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav';
 import Nine from './components/Nine';
-import Eleven from './components/Eleven';
 
 const App = () => {
-	return (
-		<>
-			<Nav></Nav>
-			<Routes>
-				<Route path="/IX" element={<Nine />}></Route>
-				<Route path="/XI" element={<Eleven />}></Route>
-				<Route path="*" element={<Navigate to="/IX" replace />} />
-			</Routes>
-		</>
-	);
+	return <Nine></Nine>;
 };
 
 export default App;
