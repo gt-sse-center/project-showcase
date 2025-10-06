fed6ab2bc585299b5d7ac397307bdefd944607c3
Varun Agrawal <varagrawal@gmail.com>
2025-10-05 23:03:34 -0400

    Use hash routing to allow SPA to work with Github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Router, Switch, Route, useLocation } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
-import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
-import MobileMenu from "@/components/layout/mobile-menu";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import Projects from "@/pages/projects";
-import ProjectDetail from "@/pages/project-detail";
+import Footer from '@/components/layout/footer';
+import Header from '@/components/layout/header';
+import MobileMenu from '@/components/layout/mobile-menu';
+import { Toaster } from '@/components/ui/toaster';
+import Home from '@/pages/home';
+import NotFound from '@/pages/not-found';
+import ProjectDetail from '@/pages/project-detail';
+import Projects from '@/pages/projects';
+import { Route, Router, Switch, useLocation } from 'wouter';
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { useHashLocation } from 'wouter/use-hash-location';
 
 function RouterLocationHook() {
   const [location] = useLocation();
@@ -38,10 +39,10 @@ function App() {
 
   return (
     <div>
-      {/* Read the `base` set in the vite.config.ts.
-          Needed for Github project pages.
+      {/* Since Vite generates a Single Page Application (SPA)
+          we need to use hash based routing
       */}
-      <Router base={import.meta.env.BASE_URL}>
+      <Router hook={useHashLocation}>
         <div className="flex flex-col min-h-screen">
           <Header toggleMobileMenu={toggleMobileMenu} />
           <MobileMenu isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} />
