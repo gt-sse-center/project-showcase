495f3a1944c8123823c6f4afd9d59715e4fed462
Varun Agrawal <varagrawal@gmail.com>
2025-10-07 02:00:09 -0400

    Add vite sitemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import eslintPlugin from "@nabla/vite-plugin-eslint";
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
+import Sitemap from "vite-plugin-sitemap";
 // for the following see https://github.com/cypress-io/cypress/issues/25397#issuecomment-1775454875
 import dns from 'dns';
 dns.setDefaultResultOrder('ipv4first');
@@ -15,6 +16,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     eslintPlugin(),
+    Sitemap({ hostname: 'https://gt-sse-center.github.io/project-showcase', outDir: "build" }),
   ],
   resolve: {  // resolve path aliases
     alias: {
