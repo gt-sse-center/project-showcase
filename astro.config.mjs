59cd59681b18593e121bd00c3ce7582679101e08
Varun Agrawal <varagrawal@gmail.com>
2025-10-07 18:08:05 -0400

    Add sitemap generation support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://gt-sse-center.github.io/project-showcase",
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [nodePolyfills(), tailwindcss()],
-  }
+  },
+  outDir: "build",
 });
